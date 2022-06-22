ec7599274c9cc4087bb41f346ed2baafb49947ee
Alex Agra <leo.agra@gmail.com>
2022-06-22 16:58:58 -0300

    Update ambiente.js

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -1,1 +1,14 @@
 var c = 1
+while (c <= 6){
+    console.log('Vamos lá?')
+    c++
+}
+
+/*
+console.log('Vamos lá?')
+console.log('Vamos lá?')
+console.log('Vamos lá?')
+console.log('Vamos lá?')
+console.log('Vamos lá?')
+console.log('Vamos lá?')
+*/
