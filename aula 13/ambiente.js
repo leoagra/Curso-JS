a7506dab324c9b2ae25eca631a25c621c3712471
Alex Agra <leo.agra@gmail.com>
2022-06-22 17:10:18 -0300

    Update ambiente.js

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -1,10 +1,18 @@
+//repetição com teste lógico no final
+var c = 1
+do{
+    console.log(`Pass0 ${c}`)
+    c++
+} while (c <= 6)
+
+/* Repetição com teste lógico no início
 var c = 1
 while (c <= 6){
     console.log('Vamos lá?')
     c++
 }
-
-/*
+*/
+/* Forma greceira
 console.log('Vamos lá?')
 console.log('Vamos lá?')
 console.log('Vamos lá?')
