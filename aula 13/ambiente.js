2c48f98b9e5cde266676f367c9e74f19fddffb92
Alex Agra <leo.agra@gmail.com>
2022-06-22 16:53:47 -0300

    Create ambiente.js

---
+++ aula 13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ // significa " c = c + 1"
+}
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
