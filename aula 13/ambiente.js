caf91e2460c7155fb0958025633beda8a9104225
Alex Agra <leo.agra@gmail.com>
2022-06-22 16:56:31 -0300

    Update ambiente.js

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -1,16 +1,1 @@
 var c = 1
-while (c <= 6) {
-    console.log('Tudo bem?')
-    c++ // significa " c = c + 1"
-}
-
-
-
-
-/*console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-*/
