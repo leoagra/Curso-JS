3e489b71fe4cfd04ef8c92cede4a19673d068deb
Alex Agra <leo.agra@gmail.com>
2022-06-28 11:02:39 -0300

    Update script.js

--- aula 14ex/script.js
+++ aula 14ex/script.js
@@ -11,20 +11,21 @@ function contar(){ //let é usado recentemente por questão de scoopo
         res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0) {
+        let p = Number(passo.value)   
+     if (p <= 0) {
             alert('Passo inválido! Considerando PASSO 1')
             p = 1
         }
         if (i < f){ //contagem crescente
-            for(let c = i; c <= f; c += p) { // a variavel "c" começa do  inicio "i", enquando o contador "c" for menor ou igual ao fim "f", o contador vai receber ele mesmo mais o passo "p"
+           for(let c = i; c <= f; c += p) { // a variavel "c" começa do  inicio "i", enquando o contador "c" for menor ou igual ao fim "f", o contador vai receber ele mesmo mais o passo "p"
                 res.innerHTML += ` ${c} \u{1F449}`
             }
         } else { // contagem regressiva
-            for(let c = i; c >= f; c -= p)
+           for(let c = i; c >= f; c -= p)
                 res.innerHTML += ` ${c} \u{1F449}`
         }
         res.innerHTML += `\u{1F3C1}` //para usar emoji no JS, ele deve estar entre crases (``), no lugar de "U+" vc coloca "\u" e o resto do codigo dentro de chaves {}
+ 
     }
 }
 
