470ac2a0c9237396f5182e5ae1d46becc636a8d5
Alex Agra <leo.agra@gmail.com>
2022-06-29 10:55:13 -0300

    tabuada

---
+++ aula 14ex/ex017/script.js
@@ -0,0 +1,19 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        alert('Por favor, digite um número')
+    }else{
+        let n = Number(num.value)
+        let c = 1 // variável criada como contador
+        tab.innerHTML = '' // para apagar a tabela antes de gerar uma nova
+        while (c <= 10){
+            let item = document.createElement('option')// para criar qualquer elemento dentro de JS eu uso "createElement('qual elemento')" 
+            item.value = `tab${c}` // serve para PHP( ainda vai saber a função)
+            item.text = `${n} + ${c} = ${n*c}`// item.text (nome da variavel.text) usa-se para adicionar os valores dentro da option.
+            tab.appendChild(item) // faz o elemento ser adicionado para dentro do desejado. ( a variável "item" foi adicionada dentro de "tab")
+            c++ // o contador propriamente dito
+        }
+    }
+    
+}
