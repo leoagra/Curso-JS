b55f0c6ed7edc7b960fbe007c44710d728c62a68
Alex Agra <leo.agra@gmail.com>
2022-07-04 12:01:16 -0300

    atualização

---
+++ aula 16/função01.js
@@ -0,0 +1,14 @@
+function parimpar(n){
+  if (n % 2 == 0) {
+    return "Par"
+  } else {
+    return "Ímpar"
+  }  
+}
+console.log(parimpar(5))
+
+/*
+---Posso ou não criar uma variável
+let res = parimpar(5)
+console.log(res)
+*/
